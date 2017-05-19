2e0b6e53ad794ce971bedbf0af52c9a9763de041
Vimukthi <vimukthi@scalable.capital>
2017-05-19 18:05:17 +0200

    debug mode flag added

--- main.go
+++ main.go
@@ -15,14 +15,16 @@ import (
 var log = logging.MustGetLogger("BEANSTALKG")
 
 func main() {
-	initLogging()
+	port := flag.String("port", "11300", "Port for beanstalkg server")
+	proxyMode := flag.Bool("proxy_mode", false, "Start server in proxy mode")
+	env := flag.String("env", "local", "Which environment config to use")
+	debugMode := flag.Bool("debug_mode", false, "Start server in debug mode. Logs shall contain more information")
+	flag.Parse()
+	initLogging(*debugMode)
 	go func() {
 		log.Info(http.ListenAndServe("localhost:6060", nil))
 	}()
-	port := flag.String("port", "11300", "Port for beanstalkg server")
-	proxy_mode := flag.Bool("proxy_mode", false, "Start server in proxy mode")
-	env := flag.String("env", "local", "Which environment config to use")
-	flag.Parse()
+
 	service := ":" + *port
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
 	checkError(err)
@@ -30,7 +32,7 @@ func main() {
 	checkError(err)
 	stop := make(chan bool)
 
-	if !*proxy_mode {
+	if !*proxyMode {
 		tubeRegister := make(chan architecture.Command)
 		// use this tube to send the channels for each individual tube to the clients when the do 'use' command
 		useTubeConnectionReceiver := make(chan chan architecture.Command)
@@ -90,7 +92,7 @@ var format = logging.MustStringFormatter(
 	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
 )
 
-func initLogging() {
+func initLogging(debugMode bool) {
 	backend1 := logging.NewLogBackend(os.Stderr, "", 0)
 	backend2 := logging.NewLogBackend(os.Stdout, "", 0)
 
@@ -101,6 +103,12 @@ func initLogging() {
 	backend1Leveled := logging.AddModuleLevel(backend1Formatter)
 	backend1Leveled.SetLevel(logging.ERROR, "")
 
+	backend2Leveled := logging.AddModuleLevel(backend2Formatter)
+	if (debugMode) {
+		backend2Leveled.SetLevel(logging.DEBUG, "")
+	} else {
+		backend2Leveled.SetLevel(logging.INFO, "")
+	}
 	// Set the backends to be used.
-	logging.SetBackend(backend1Leveled, backend2Formatter)
+	logging.SetBackend(backend1Leveled, backend2Leveled)
 }
