8958c74b2941048c334b5d3f5e5a0cb44729b654
Steven Edwards <steven@stephenwithav.com>
2017-05-23 05:02:47 -0400

    min_heap_test is now table-driven.

--- backend/min_heap_test.go
+++ backend/min_heap_test.go
@@ -8,73 +8,55 @@ import (
 	"github.com/vimukthi-git/beanstalkg/architecture"
 )
 
-/**
-5
-INSERT 4
-INSERT 9
-DELETE 4
-*/
-func TestMinHeap_Insert(t *testing.T) {
-	m := MinHeap{}
-	m.Enqueue(ownHeapItem{4, string(1), time.Now().UnixNano()})
-	fmt.Println(m.Min().Key())
-	if m.Min().Key() != 4 {
-		t.Fail()
-	}
-	m.Enqueue(ownHeapItem{9, string(2), time.Now().UnixNano()})
-	fmt.Println(m.Min())
-	if m.Min().Key() != 4 {
-		t.Fail()
-	}
-	m.Delete(string(1))
-	if m.Size() != 1 {
-		t.Fail()
-	}
-	fmt.Println(m.Min().Key())
-	if m.Min().Key() != 9 {
-		t.Fail()
-	}
-	// m.Delete(string(2))
-	fmt.Println(m.Dequeue().Key(), string(3))
+const numberOfInserts = 3
+
+var tt = []architecture.PriorityQueueItem{
+	ownHeapItem{4, "1", time.Now().UnixNano()},
+	ownHeapItem{9, "2", time.Now().UnixNano()},
+	ownHeapItem{3, "3", time.Now().UnixNano()},
+
+	ownHeapItem{1, "one", time.Now().UnixNano()},
+	ownHeapItem{1, "two", time.Now().UnixNano()},
+	ownHeapItem{1, "three", time.Now().UnixNano()},
+	ownHeapItem{1, "four", time.Now().UnixNano()},
 }
 
-func TestMinHeap_InsertCheckDelete(t *testing.T) {
+func TestMinHeap_Insert(t *testing.T) {
 	m := MinHeap{}
-	m.Enqueue(ownHeapItem{1, "one", time.Now().UnixNano()})
-	m.Enqueue(ownHeapItem{1, "two", time.Now().UnixNano()})
-	m.Enqueue(ownHeapItem{1, "three", time.Now().UnixNano()})
-	m.Enqueue(ownHeapItem{1, "four", time.Now().UnixNano()})
-	fmt.Println(m)
-	item := m.Dequeue()
-	if item.Id() != "one" {
-		t.Fail()
+
+	var expectedMins = []int64{4, 4, 3}
+	var tests = tt[:numberOfInserts]
+
+	// Test inserting items into heap.
+	for i, testItem := range tests {
+		m.Enqueue(testItem)
+
+		if key := m.Min().Key(); key != expectedMins[i] {
+			t.Errorf("Expected %d, got %d.", expectedMins[i], key)
+		}
 	}
-	fmt.Println(item, m)
-	item = m.Dequeue()
-	if item.Id() != "two" {
-		t.Fail()
+}
+
+func TestMinHeap_Dequeue(t *testing.T) {
+	m := MinHeap{}
+
+	var tt_expectedIds = []string{"one", "two", "three", "four"}
+
+	for _, testItem := range tt[numberOfInserts:] {
+		m.Enqueue(testItem)
 	}
-	fmt.Println(item, m)
-	item = m.Dequeue()
-	if item.Id() != "three" {
-		t.Fail()
+
+	for _, expectedId := range tt_expectedIds {
+		item := m.Dequeue()
+
+		if id := item.Id(); id != expectedId {
+			t.Errorf("Expected %s, got %s.", expectedId, id)
+		}
 	}
-	fmt.Println(item, m)
-	item = m.Dequeue()
-	if item.Id() != "four" {
-		t.Fail()
+
+	if item := m.Dequeue(); item != nil {
+		t.Errorf("Expected nil, got %#v", item)
 	}
-	fmt.Println(item, m)
-	if m.Dequeue() != nil {
-		t.Fail()
-	}
-	m.Enqueue(ownHeapItem{1, "one", time.Now().UnixNano()})
-	item = m.Dequeue()
-	if item.Id() != "one" {
-		t.Fail()
-	}
-	//item = m.Dequeue().(ownHeapItem)
-	//fmt.Println(item, m)
 }
 
 func TestIntegration(t *testing.T) {
@@ -83,13 +65,13 @@ func TestIntegration(t *testing.T) {
 	fmt.Println(tube)
 	tube.Delayed.Enqueue(ownHeapItem{4, string(1), time.Now().UnixNano()})
 	if tube.Delayed.Dequeue().Key() != 4 {
-		t.Fail()
+		t.Error("invalid key")
 	}
 	fmt.Println(tube.Delayed)
 	if tube.Delayed.Find(string(1)) != nil {
-		t.Fail()
+		t.Error("delayed.find failed")
 	}
 	if tube.Delayed.Dequeue() != nil {
-		t.Fail()
+		t.Error("delayed.dequeue failed")
 	}
 }
