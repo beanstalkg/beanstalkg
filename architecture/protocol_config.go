05a6cfb0dde391ff1e1e307be913a4ca2e2deee0
marek <marek.narozniak@gmail.com>
2017-06-03 11:21:22 +0200

    introducing initializer function that allocates array of arbitrary type function

--- architecture/protocol_config.go
+++ architecture/protocol_config.go
@@ -23,89 +23,97 @@ var cmdParseOptions map[CommandName]CommandParseOptions
 var cmdReplyOptions map[CommandName]CommandReplyOptions
 
 func identity(argument string) string { return argument }
+func initializer(arguments []interface{}) []interface{} {
+	result := make([]interface{}, len(arguments))
+	result = arguments
+	return result
+}
 
 func init() {
-
-	funcs := make([]interface{}, 1)
-	funcs = []interface{}{ identity }
-
-	funcs2 := make([]interface{}, 4)
-	funcs2 = []interface{}{ strconv.Atoi, strconv.Atoi, strconv.Atoi, strconv.Atoi }
-
 	cmdParseOptions = map[CommandName]CommandParseOptions{
 		USE: {
 			Name:           USE,
 			ExpectedLength: 2,
 			WaitingForMore: false,
 			Params:         []string{"tube"},
-			ParamValidators: funcs,
+			ParamValidators: initializer( []interface{}{ identity } ),
 		},
 		PUT: {
 			Name:           PUT,
 			ExpectedLength: 5,
 			WaitingForMore: true,
 			Params:         []string{"pri", "delay", "ttr", "bytes"},
-			ParamValidators: funcs2,
+			ParamValidators: initializer( []interface{}{ strconv.Atoi, strconv.Atoi, strconv.Atoi, strconv.Atoi } ),
 		},
 		WATCH: {
 			Name:           WATCH,
 			ExpectedLength: 2,
 			WaitingForMore: false,
 			Params:         []string{"tube"},
+			ParamValidators: initializer( []interface{}{ identity } ),
 		},
 		IGNORE: {
 			Name:           IGNORE,
 			ExpectedLength: 2,
 			WaitingForMore: false,
 			Params:         []string{"tube"},
+			ParamValidators: initializer( []interface{}{ identity } ),
 		},
 		RESERVE: {
 			Name:           RESERVE,
 			ExpectedLength: 1,
 			WaitingForMore: false,
 			Params:         []string{},
+			ParamValidators: initializer( []interface{}{} ),
 		},
 		RESERVE_WITH_TIMEOUT: {
 			Name:           RESERVE_WITH_TIMEOUT,
 			ExpectedLength: 2,
 			WaitingForMore: false,
 			Params:         []string{"timeout"},
+			ParamValidators: initializer( []interface{}{ strconv.Atoi } ),
 		},
 		DELETE: {
 			Name:           DELETE,
 			ExpectedLength: 2,
 			WaitingForMore: false,
 			Params:         []string{"id"},
+			ParamValidators: initializer( []interface{}{ identity } ),
 		},
 		RELEASE: {
 			Name:           RELEASE,
 			ExpectedLength: 4,
 			WaitingForMore: false,
 			Params:         []string{"id", "pri", "delay"},
+			ParamValidators: initializer( []interface{}{ identity, strconv.Atoi, strconv.Atoi } ),
 		},
 		BURY: {
 			Name:           BURY,
 			ExpectedLength: 3,
 			WaitingForMore: false,
 			Params:         []string{"id", "pri"},
+			ParamValidators: initializer( []interface{}{ identity, strconv.Atoi } ),
 		},
 		TOUCH: {
 			Name:           TOUCH,
 			ExpectedLength: 2,
 			WaitingForMore: false,
 			Params:         []string{"id"},
+			ParamValidators: initializer( []interface{}{ identity } ),
 		},
 		KICK: {
 			Name:           KICK,
 			ExpectedLength: 2,
 			WaitingForMore: false,
 			Params:         []string{"bound"},
+			ParamValidators: initializer( []interface{}{ strconv.Atoi } ),
 		},
 		KICK_JOB: {
 			Name:           KICK_JOB,
 			ExpectedLength: 2,
 			WaitingForMore: false,
 			Params:         []string{"id"},
+			ParamValidators: initializer( []interface{}{ identity } ),
 		},
 	}
 
