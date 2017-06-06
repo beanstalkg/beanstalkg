2dc67382ebaba3938c1a1fcb37e767711767f0dd
Vimukthi <vbwickramasinghe@gmail.com>
2017-06-06 13:15:51 +0200

    Integration tests iteration 1 (#76)
    * #54 #57 created the first integration tests
    
    * #54 #57 created the first integration tests
    
    * #54 #57 minor stuff

---
+++ testintegration/put_test.go
@@ -0,0 +1,45 @@
+package testintegration
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+	"os/exec"
+	"time"
+)
+
+var _ = Describe("Put", func() {
+
+	var bs_session *gexec.Session
+	var conn *Conn
+
+	BeforeEach(func() {
+		var err error
+		command := exec.Command("../beanstalkg")
+		bs_session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
+		Ω(err).ShouldNot(HaveOccurred())
+		// wait for the server to become ready
+		time.Sleep(500 * time.Millisecond)
+		conn, err = dial("tcp", "127.0.0.1:11300")
+		Ω(err).ShouldNot(HaveOccurred())
+	})
+
+	Describe("Put command", func() {
+		Context("Used with default tube", func() {
+			It("should correctly put first job", func() {
+				r, err := conn.cmd([]byte("hello"), "put", 0, dur(1), dur(1))
+				Ω(err).ShouldNot(HaveOccurred())
+				var id string
+				_, err = conn.readResp(r, false, "INSERTED %s", &id)
+				Ω(err).ShouldNot(HaveOccurred())
+				println("id is " + id)
+				Ω(id).Should(MatchRegexp("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"))
+			})
+		})
+	})
+
+	AfterEach(func() {
+		gexec.Kill()
+		conn.Close()
+	})
+})
