85042b659fe17081a3345196a0129239d78b41a6
Steven Edwards <steven@stephenwithav.com>
2017-06-06 02:43:14 -0400

    Update imports to new repo location. (#75)

--- operation/client_handler.go
+++ operation/client_handler.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 
 	"github.com/op/go-logging"
-	"github.com/vimukthi-git/beanstalkg/architecture"
+	"github.com/beanstalkg/beanstalkg/architecture"
 )
 
 var log = logging.MustGetLogger("BEANSTALKG")
