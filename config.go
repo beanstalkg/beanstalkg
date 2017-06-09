f74484a4499aa18efa79254e1b74cbab487934dd
Steven Edwards <steven@stephenwithav.com>
2017-06-09 15:12:28 -0400

    Implement server configuration with configor.
    configor will state that configuration files are missing if none are
    available, but it won't return an error.
    
    getConfig sets values based on the following order of precedence: flags,
    environment variables, configuration files, and finally defaults.
    
    Added environment variables: BEANSTALKG_PORT, BEANSTALKG_DEBUG.

---
+++ config.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/jinzhu/configor"
+)
+
+type ServerConfig struct {
+	Port  string `default:"11300"`
+	Debug bool
+}
+
+// getConfig sets values based on the following order of precedence:
+// flags, environment variables, configuration files, and finally
+// defaults.
+func getConfig() *ServerConfig {
+	cfg := &ServerConfig{}
+	configor.New(&configor.Config{ENVPrefix: "BEANSTALKG"}).Load(cfg, "config.yml")
+
+	flag.StringVar(&cfg.Port, "port", cfg.Port, "Port for beanstalkg server")
+	flag.BoolVar(&cfg.Debug, "debug", cfg.Debug, "Start server in debug mode. Logs shall contain more information")
+	flag.Parse()
+
+	return cfg
+}
