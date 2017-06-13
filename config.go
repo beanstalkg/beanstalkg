fa44d4f4cad7e1d5f9053f24b0a253ef5e8da5de
Steven Edwards <steven@stephenwithav.com>
2017-06-13 04:20:08 -0400

    Backend is now configurable via the command line.

--- config.go
+++ config.go
@@ -20,6 +20,7 @@ func getConfig() *ServerConfig {
 	configor.New(&configor.Config{ENVPrefix: "BEANSTALKG"}).Load(cfg, "config.yml")
 
 	flag.StringVar(&cfg.Port, "port", cfg.Port, "Port for beanstalkg server")
+	flag.StringVar(&cfg.Backend, "backend", cfg.Backend, "Use this backend for job storage.")
 	flag.BoolVar(&cfg.Debug, "debug", cfg.Debug, "Start server in debug mode. Logs shall contain more information")
 	flag.Parse()
 
